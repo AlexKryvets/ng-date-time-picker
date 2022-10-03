6fc2e6f38b2591704c379b594ce4e67330d9331d
alex.kryvets <alex.kryvets@gmail.com>
2022-10-03 13:52:15 +0300

    add datepicker example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule],
+    imports: [BrowserModule, BrowserAnimationsModule, MatInputModule, MatNativeDateModule, MatDatepickerModule],
     providers: [],
     bootstrap: [AppComponent],
 })
