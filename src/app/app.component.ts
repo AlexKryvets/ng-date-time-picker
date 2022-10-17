991eef8e7ba4a2fa66f4cd86bac07d29d70461bd
Alexey Kryvets <alex.kryvets@gmail.com>
2022-10-17 14:28:26 +0300

    made changes in demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'date-time-picker-app';
+  title = 'date-time-picker-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {NgDateTimePickerModule} from 'ng-date-time-picker';
+import {NgDateTimePickerModule, NgMatNativeDateModule} from 'ng-date-time-picker';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatInputModule,
-        MatNativeDateModule,
-        MatDatepickerModule,
-        NgDateTimePickerModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    NgDateTimePickerModule,
+    NgMatNativeDateModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
