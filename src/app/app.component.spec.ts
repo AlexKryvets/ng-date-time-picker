d795478f2cdb493194664fe35e70ab442cc9062b
Oleksii Kryvets <alex.kryets@gmail.com>
2024-02-05 19:53:01 +0200

    1. update deps 2. fix datetimepicker styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it('should have as title \'date-time-picker-app\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('date-time-picker-app');
+    expect(app.stepMinute).toEqual(10);
   });
 
   it('should render title', () => {
