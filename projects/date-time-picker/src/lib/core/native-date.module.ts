e15b624931a20b84e8932652d63394068f2ce761
Oleksii Kryvets <alex.kryets@gmail.com>
2024-02-08 10:34:14 +0200

    fix deps

--- projects/date-time-picker/src/lib/core/native-date.module.ts
+++ projects/date-time-picker/src/lib/core/native-date.module.ts
@@ -6,24 +6,36 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { PlatformModule } from '@angular/cdk/platform';
-import { NgModule } from '@angular/core';
-import { NgMatDateAdapter } from './date-adapter';
-import { NgMatNativeDateAdapter } from './native-date-adapter';
-import { NG_MAT_NATIVE_DATE_FORMATS } from './native-date-formats';
-import { NG_MAT_DATE_FORMATS } from './date-formats';
+// import {PlatformModule} from '@angular/cdk/platform';
+import {MatDateFormats} from '@angular/material/core';
+import {NgModule, Provider} from '@angular/core';
+import {NgMatDateAdapter} from './date-adapter';
+import {NgMatNativeDateAdapter} from './native-date-adapter';
+import {NG_MAT_NATIVE_DATE_FORMATS} from './native-date-formats';
+import {NG_MAT_DATE_FORMATS} from './date-formats';
 
 
 @NgModule({
-  imports: [PlatformModule],
+  // imports: [PlatformModule],
   providers: [
-    { provide: NgMatDateAdapter, useClass: NgMatNativeDateAdapter },
+    {provide: NgMatDateAdapter, useClass: NgMatNativeDateAdapter},
   ],
 })
-export class NgNativeDateModule { }
+export class NgNativeDateModule {
+}
 
 @NgModule({
-  imports: [NgNativeDateModule],
-  providers: [{ provide: NG_MAT_DATE_FORMATS, useValue: NG_MAT_NATIVE_DATE_FORMATS }],
+  // imports: [NgNativeDateModule],
+  providers: [provideNgNativeDateAdapter()],
 })
-export class NgMatNativeDateModule { }
+export class NgMatNativeDateModule {
+}
+
+export function provideNgNativeDateAdapter(
+  formats: MatDateFormats = NG_MAT_NATIVE_DATE_FORMATS,
+): Provider[] {
+  return [
+    {provide: NgMatDateAdapter, useClass: NgMatNativeDateAdapter},
+    {provide: NG_MAT_DATE_FORMATS, useValue: formats},
+  ];
+}
